0061fafd994cdcb841175f18de91609e6549f383
Carrie Roberts <you@exmaple.com>
2020-02-06 23:18:28 -0700

    DisablePrivacyExperience

--- script.ps1
+++ script.ps1
@@ -1,2 +1,6 @@
 Get-LocalUser -Name "art" | Enable-LocalUser
 New-Item -Path "C:\Users\art\Desktop\youdidit.txt" -ItemType File
+$p = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OOBE"
+$n = "DisablePrivacyExperience"
+New-Item -Path $p –Force
+New-ItemProperty -Path $p -Name $n -Value 1 -Type DWORD -Force
