40f7975d5098db9c846e2f1ccc2118c42cf5e612
Carrie Roberts <you@exmaple.com>
2020-02-06 22:30:32 -0700

    add file to desktop

--- script.ps1
+++ script.ps1
@@ -1,1 +1,3 @@
-Get-LocalUser -Name "art1of8" | Enable-LocalUser
+Get-LocalUser -Name "art" | Enable-LocalUser
+$fn = join-path  ([Environment]::GetFolderPath("Desktop")) "youdidit.txt"
+New-Item -Path $fn -ItemType File
