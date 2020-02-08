939e9e4230d2c26d63035dbb404514dceba9afec
Carrie Roberts <you@exmaple.com>
2020-02-08 09:29:41 -0700

    extionsion script added

--- script.ps1
+++ script.ps1
@@ -1,6 +1,18 @@
-Get-LocalUser -Name "art" | Enable-LocalUser
-New-Item -Path "C:\Users\art\Desktop\youdidit.txt" -ItemType File
-$p = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OOBE"
-$n = "DisablePrivacyExperience"
-New-Item -Path $p -Force
-New-ItemProperty -Path $p -Name $n -Value 1 -Type DWORD -Force
+$user = "art-admin"
+
+function Disable-OOBE {
+    $p = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OOBE"
+    $n = "DisablePrivacyExperience"
+    New-Item -Path $p -Force
+    New-ItemProperty -Path $p -Name $n -Value 1 -Type DWORD -Force
+}
+
+function Add-DesktopShortCutsToDefaultProfile {
+
+Copy-Item "C:\Users\$user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\Command Prompt.lnk" "C:\Users\Default\Desktop\Command Prompt.lnk"
+Copy-Item "C:\Users\$user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" "C:\Users\Default\Desktop\Window PowerShell.lnk"
+
+}
+
+Disable-OOBE
+Add-DesktopShortCutsToDefaultProfile
