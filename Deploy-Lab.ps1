854e47f58288d2a5c5a9c4b4f351f66dba629bb8
Carrie Roberts <clr2of8@gmail.com>
2020-08-22 06:19:12 -0600

    first checking with scythe server

--- Deploy-Lab.ps1
+++ Deploy-Lab.ps1
@@ -4,7 +4,7 @@ function Deploy-Lab {
 
         [Parameter(Mandatory = $false)]
         [string]
-        $resourceGroupName = "art-lab",
+        $resourceGroupName = "d-art-lab",
         
         [Parameter(Mandatory = $false)]
         [Int]
@@ -23,8 +23,8 @@ function Deploy-Lab {
         if ($null -eq (get-azresourcegroup | Where-Object -Property "ResourceGroupName" -eq $resourceGroupName)) {
             New-AzResourceGroup -Name  $resourceGroupName -Location "East US"
         }
-        $templateFile = "template.json"
-        $parameterFile = "parameters.json"
+        $templateFile = ".\windows-client\template.json"
+        $parameterFile = ".\windows-client\parameters.json"
         $startTime = Get-Date; Write-Host -ForegroundColor yellow "Start Time: $startTime"
         New-AzResourceGroupDeployment -Name "initialDeploy" -ResourceGroupName $resourceGroupName -TemplateFile $templateFile -Count $numVMs -templateParameterFile $parameterFile
         $endTime = Get-Date; Write-Host -ForegroundColor green "End Time: $endTime"
@@ -41,7 +41,7 @@ function Deploy-Lab {
         $batchSize = 5
         $delayBetweenBatches = 20
         $count = 1
-        $cred = Get-Content "pcs.txt" # must create this file in directory where you are executing Deploy-Lab, contains the 'art' user RDP password
+        $cred = Get-Content "windows-client\pcs.txt" # must create this file in directory where you are executing Deploy-Lab, contains the 'art' user RDP password
         foreach ($ip in $ips.IpAddress) {
             start-process -FilePath "powershell.exe" -ArgumentList "-exec bypass & {
             `$User = \`"art\`"
@@ -58,6 +58,52 @@ function Deploy-Lab {
         }
     }
     
+    Write-Host -ForegroundColor Green "Destroy this resource group with the following command:"
+    Write-Host -ForegroundColor Cyan "Remove-AzResourceGroup -ResourceGroupName $resourceGroupName"
+}
+
+function Deploy-Scythe {
+
+    Param(
+
+        [Parameter(Mandatory = $false)]
+        [string]
+        $resourceGroupName = "d-scythe-server",
+        
+        [Parameter(Mandatory = $false)]
+        [ValidateRange(1,20)]
+        [Int]
+        $numVMs=1,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$noDeploy = $false,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$noRDP = $false
+
+    )
+
+    # This creates the resources group if needed and then deploys the template to it to create the number of VM's specified in the '-numVMs'
+    if (-not $noDeploy) {
+        if ($null -eq (get-azresourcegroup | Where-Object -Property "ResourceGroupName" -eq $resourceGroupName)) {
+            New-AzResourceGroup -Name  $resourceGroupName -Location "East US"
+        }
+        $templateFile = ".\scythe-server\scythe-server-template.json"
+        $parameterFile = ".\scythe-server\scythe-server-parameters.json"
+        $startTime = Get-Date; Write-Host -ForegroundColor yellow "Start Time: $startTime"
+        New-AzResourceGroupDeployment -Name "initialDeploy" -ResourceGroupName $resourceGroupName -TemplateFile $templateFile -Count $numVMs -templateParameterFile $parameterFile
+        $endTime = Get-Date; Write-Host -ForegroundColor green "End Time: $endTime"
+        $elapsed = ($endTime - $startTime).TotalMinutes
+        Write-Host -ForegroundColor Cyan "Elapsed: $elapsed minutes"
+    }
+    
+    # just print out all the IP addresses for the VM's in this resource group
+    $ips = Get-AzPublicIpAddress -ResourceGroupName $resourceGroupName | Select-Object -Property "IpAddress"
+    $ips | ForEach-Object {
+        Write-Host -ForegroundColor Magenta $_.IpAddress
+        Write-Host -ForegroundColor yellow "https://$($_.IpAddress):8443" 
+        }
+   
     Write-Host -ForegroundColor Green "Destroy this resource group with the following command:"
     Write-Host -ForegroundColor Cyan "Remove-AzResourceGroup -ResourceGroupName $resourceGroupName"
 }
