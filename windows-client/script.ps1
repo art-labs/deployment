12906319071472539d6ae357dc6d48d0501332ed
Carrie Roberts <clr2of8@gmail.com>
2020-08-28 16:36:39 -0600

    sleep 10

--- windows-client/script.ps1
+++ windows-client/script.ps1
@@ -53,7 +53,7 @@ function Set-LabBookmark ($labsURL) {
 function Set-ChromeAsDefaultBrowser {
     Add-Type -AssemblyName 'System.Windows.Forms'
     Start-Process $env:windir\system32\control.exe -ArgumentList '/name Microsoft.DefaultPrograms /page pageDefaultProgram\pageAdvancedSettings?pszAppName=google%20chrome'
-    Sleep 2
+    Sleep 10
     [System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB} {ENTER}{ENTER}")
 }
 
