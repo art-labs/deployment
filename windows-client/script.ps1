9a3a0be9a3f6932439a07f6625486cc2a4db0a10
Carrie Roberts <clr2of8@gmail.com>
2020-08-28 16:15:08 -0600

    set chrome as default browser

--- windows-client/script.ps1
+++ windows-client/script.ps1
@@ -50,6 +50,13 @@ function Set-LabBookmark ($labsURL) {
     } | Set-Content -nonewline
 }
 
+function Set-ChromeAsDefaultBrowser {
+    Add-Type -AssemblyName 'System.Windows.Forms'
+    Start-Process $env:windir\system32\control.exe -ArgumentList '/name Microsoft.DefaultPrograms /page pageDefaultProgram\pageAdvancedSettings?pszAppName=google%20chrome'
+    Sleep 2
+    [System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB} {ENTER}{ENTER}")
+}
+
 # Disable-OOBE
 # Desktop shortcuts are already part of the image
 # Add-DesktopShortCutsToDefaultProfile
@@ -57,3 +64,4 @@ function Set-LabBookmark ($labsURL) {
 #Disable-NetworkDiscovery
 Get-BookMarks
 Set-LabBookmark $labsURL
+Set-ChromeAsDefaultBrowser
